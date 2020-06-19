f41b47f23bc5f8601eb82aba1593fe6146687e2d
lapkaK0sh34ki <66837221+lapkaK0sh34ki@users.noreply.github.com>
2020-06-20 01:07:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var messages = document.getElementById("messages");
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+    var newMessages = document.createElement("li");
+    newMessages.innerHTML = textbox.value;
+    messages.appendChild(newMessage);
+    textbox.value = "";
+});
+
