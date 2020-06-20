880428dd61ac8bb2b115c0691fa118bd9174d590
lapkaK0sh34ki <66837221+lapkaK0sh34ki@users.noreply.github.com>
2020-06-20 03:31:51 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,13 +8,6 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
-
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
