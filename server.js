39f1ed88336d16856851ab929bb856ac414b172a
lapkaK0sh34ki <66837221+lapkaK0sh34ki@users.noreply.github.com>
2020-06-20 03:28:09 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const io = require('socket.io')(3000)
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-const users = {}
+server.listen(3000);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
